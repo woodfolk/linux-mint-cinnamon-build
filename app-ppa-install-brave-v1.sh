7d605c3f712b9f0ebb31c335b425a6f68e67ecf0
woodfolk <woodfolk@gmail.com>
2019-04-05 06:43:04 -0400

    Fixed Repo Added code in script

--- app-ppa-install-brave-v1.sh
+++ app-ppa-install-brave-v1.sh
@@ -5,8 +5,8 @@ set -e
 # Name:			app-ppa-install-brave-v1.sh
 # Author:		Romano Woodfolk
 # Created:		March 15, 2019
-# Modified:		March 19, 2019 (110100100)
-# Version:		1.0.1
+# Modified:		April 05, 2019 (110100100)
+# Version:		1.0.2
 # Website: 		http://www.romanowoodfolk.com 
 #---------------------------------------------------------------------------------#
 # Comments: This script contains the installation commands for Linux Mint 19.1    #
@@ -15,38 +15,41 @@ set -e
 # http://ubuntuhandbook.org/index.php/2018/12/how-to-install-brave-web-browser-in-ubuntu-linux-mint/
 #
 echo -e "------------------------------------------------------------------"
-echo -e " Creating Log files..."
+echo -e " Installing Brave Web Browser..."
 echo -e "------------------------------------------------------------------"
 # Defining Script Variables 
 	SCRPTNAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")" 
 	SCRPTNOW=`date +%Y%m%d%H%M%S`													# Current Date & Time Suffix
 	SCRPTLOGFILE="$SCRPTNAME"_instlog_"$SCRPTNOW".log						# Script Log File
 	SCRPTERRORFILE="$SCRPTNAME"_errlog_"$SCRPTNOW".log						# Error Log File
-# Creating Log Files
-	touch $SCRPTERRORFILE
-	exec 2> $SCRPTERRORFILE
+
+
 echo -e "------------------------------------------------------------------"
 echo -e " Log files created..."
 echo -e "------------------------------------------------------------------"
-
-
-echo -e ""; clear; echo -e ""														# clear Screen
-echo -e "------------------------------------------------------------------"
-echo -e " Installing Brave Web Browser..."
-echo -e "------------------------------------------------------------------"
+# Creating Log Files
+	touch $SCRPTERRORFILE
+	exec 2> $SCRPTERRORFILE
 
 
 echo -e "------------------------------------------------------------------"
-echo -e " Installing Brave Web Browser..."
+echo -e " Installing Brave Web Browser Repository Key..."
 echo -e "------------------------------------------------------------------"
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 
 
+echo -e ""; clear; echo -e ""														# clear Screen
 echo -e "------------------------------------------------------------------"
-echo -e " Installing Brave Web Browser..."
+echo -e " Adding Brave Web Browser Repository"
 echo -e "------------------------------------------------------------------"
-source /etc/os-release
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ bionic main" | sudo tee /etc/apt/sources.list.d/brave-browser-release-bionic.list
+sudo sh -c 'echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com bionic main" >> /etc/apt/sources.list.d/brave.list'
+
+
+##echo -e "------------------------------------------------------------------"
+##echo -e " Installing Brave Web Browser..."
+##echo -e "------------------------------------------------------------------"
+##source /etc/os-release
+##echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ bionic main" | sudo tee /etc/apt/sources.list.d/brave-browser-release-bionic.list
 
 
 echo -e "------------------------------------------------------------------"
